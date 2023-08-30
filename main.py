bf6efed61584dc383ef0a5a03e8e336133bb8c03
logicofgod <dapp.prize@gmail.com>
2023-08-30 19:02:47 +0800

    origin

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ spotsList = pd.read_csv('./data/spot_list/spotsList_clean.csv')
 getTime = '1 年前'
 wrong = []
 
-for index, row in spotsList[558:600].iterrows():
+for index, row in spotsList[100:150].iterrows():
     print(row['name'])
     # =====================================================
     # Google 爬蟲
